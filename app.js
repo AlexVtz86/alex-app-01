2d1d8e562339fd6b794cc75aecddab2d2d02c678
gal <gal.oiring@gmail.com>
2024-07-02 10:13:53 +0300

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const path = require('path');
-const dataRoutes = require('./ROUTES/dataRoutes');
+const express = require("express");
+const path = require("path");
+const dataRoutes = require("./ROUTES/dataRoutes");
 
 const app = express();
 
+// Middleware
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api', dataRoutes);
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+// Static file serving
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/data", express.static(path.join(__dirname, "data")));
+
+// Routes
+app.use("/api", dataRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
